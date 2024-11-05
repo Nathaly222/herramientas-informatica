1165a93c72bd8fef9ecb740742817534827ede38
Nathaly222 <146012550+Nathaly222@users.noreply.github.com>
2024-11-05 18:59:07 -0500

    Delete ecoTecRecicla directory

--- ecoTecRecicla/src/main/kotlin/com/example/ecoTecRecicla/repository/ReportRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.example.ecoTecRecicla.repository
-
-import com.example.ecoTecRecicla.entity.Report
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface ReportRepository: JpaRepository<Report, Long>{
-
-}
--- ecoTecRecicla/src/main/kotlin/com/example/ecoTecRecicla/repository/UserRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.example.ecoTecRecicla.repository
-
-import com.example.ecoTecRecicla.entity.User
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface UserRepository:JpaRepository<User, Long>{
-}
